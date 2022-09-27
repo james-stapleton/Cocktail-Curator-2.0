94e70218b1eb63bfbb9051b02b73ae35dc375d81
“jms” <jstapleton536@gmail.com>
2022-09-27 12:35:44 -0400

    get and create cocktail routes

--- controllers/api/cocktail-routes.js
+++ controllers/api/cocktail-routes.js
@@ -1,4 +1,41 @@
 const router = require('express').Router();
+const {Cocktails, Users, UserCocktails} = require('../../models');
+
+
+//cocktail get routes
+router.get('/', async (req,res) => {
+    try {
+        const cocktailData = await Cocktails.findAll();
+        res.status(200).json(cocktailData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const cocktailData = await Cocktails.findByPk(req.params.id);
+        if (!cocktailData) {
+            res.status(404).json({message: "No cocktail with that id"});
+        }
+        else {
+        res.status(200).json(cocktailData);
+    }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//cocktail create route
+router.post('/', async (req, res) => {
+    try {
+        const cocktailData = await Cocktails.create(req.body);
+        res.status(200).json(cocktailData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}); 
+
 
 
 module.exports = router;
