a74b7a50bcfd079f33e19b844b85568fb3b2abd9
“jms” <jstapleton536@gmail.com>
2022-09-27 11:11:48 -0400

    user update route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,24 +2,39 @@ const router = require('express').Router();
 const { use } = require('passport');
 const { Users, Cocktails, UserCocktails } = require('../../models');
 
-router.get('/', (req,res) => {
-    Users.findAll( {include: Cocktails, through: UserCocktails})
-    .then((userData) => {
-        if (!userData) {
-            res.json({message: "No users found"});
-        }
-        res.json(userData);
-}).catch(err => res.send(err));
+
+// User get methods
+router.get('/', async (req,res) => {
+    try {
+        const userData = await Users.findAll({include: Cocktails, through: UserCocktails});
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.get('/:id', (req,res) => {
-    Users.findByPk(req.params.id, {include: Cocktails, through: UserCocktails })
-    .then((userData) => {
-        if (!userData) {
-            res.json({message: "No user found by that id"});
-        }
-        res.json(userData);
-}).catch(err => res.send(err));
+router.get('/:id', async (req,res) => {
+    try {
+        const userData = await Users.findByPk( req.params.id, {include: Cocktails, through: UserCocktails } );
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
+
+//User create method
+router.post('/', async (req,res) => {
+    try {
+        const userData = await Users.create(req.body);
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//User update method
+router.put('/:id', async (req,res) => {
+    
+})
 
 module.exports = router;
