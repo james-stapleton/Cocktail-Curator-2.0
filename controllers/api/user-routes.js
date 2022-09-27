c36491881509957b72eb2ef864a6924deb63b786
“jms” <jstapleton536@gmail.com>
2022-09-27 12:21:06 -0400

    user delete route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -42,4 +42,20 @@ router.put('/:id', async (req,res) => {
     }
 });
 
+//User delete method
+router.delete('/:id', async (req, res) => {
+    try {
+      const userData = await Users.destroy({
+        where: { id: req.params.id }
+      });
+      if (!userData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
