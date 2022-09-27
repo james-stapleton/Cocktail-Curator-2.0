1356c057d19e280436468001e9d9bc4539e56080
“jms” <jstapleton536@gmail.com>
2022-09-27 10:23:59 -0400

    user get routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const { use } = require('passport');
+const { Users, Cocktails, UserCocktails } = require('../../models');
 
-const userRoutes = require('./user-routes');
+router.get('/', (req,res) => {
+    Users.findAll( {include: Cocktails, through: UserCocktails})
+}).then((userData) => {
+    if (!userData) {
+        res.send(404).json({message: "No users found"});
+    }
+    res.send(200).json(userData);
+}).catch(err => res.send(err));
 
-router.use('/users', userRoutes);
-
-module.exports = router;
+router.get('/:id', (req,res) => {
+    Users.findByPk(req.params.id, {include: Cocktails, through: UserCocktails })
+}).then((userData) => {
+    if (!userData) {
+        res.send(404).json({message: "No user found by that id"});
+    }
+    res.send(200).json(userData);
+}).catch(err => res.send(err));
