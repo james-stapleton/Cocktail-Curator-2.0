a0a5a6e3b3a831a1d72bc63304bd3ea0de4587fb
“jms” <jstapleton536@gmail.com>
2022-09-27 14:01:01 -0400

    all basic routes

--- models/UserCocktails.js
+++ models/UserCocktails.js
@@ -6,12 +6,6 @@ const Users = require('./Users');
 class UserCocktails extends Model {} 
     UserCocktails.init (
         {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true
-         },
         // user_id: {
         //     type: DataTypes.INTEGER,
         //     allowNull: false,
