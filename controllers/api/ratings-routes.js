e38632a45e2161e7d83599bafe3cc0d551c597a9
“jms” <jstapleton536@gmail.com>
2022-09-28 15:43:50 -0400

    create new rating route

--- controllers/api/ratings-routes.js
+++ controllers/api/ratings-routes.js
@@ -11,4 +11,13 @@ router.get('/', async (req,res) => {
     }
 });
 
+router.post('/', async (req,res) => {
+    try {
+        const newRating = Ratings.create(req.body);
+        res.status(200).json(newRating);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
