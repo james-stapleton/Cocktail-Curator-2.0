79eeef8fb3920cd5ad5977c073991029b57b84c7
James Stapleton <jstapleton536@gmail.com>
2022-09-29 13:48:29 -0400

    weather function

--- server.js
+++ server.js
@@ -21,32 +21,31 @@ app.use(cors());
 //set static path
 //require use of specif routes
 
-const hbs = exphbs.create({ helpers });
-app.engine('handlebars', exphbs({defaultLayout: "main", layoutsDir: path.join(__dirname, "views/layouts")}));
-app.set("view engine", "handlebars")
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.render('main', {layout : 'userDrinks'});
-  });
+// app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main"}));
 
 
+// app.engine('handlebars', exphbs({defaultLayout: "main", layoutsDir: path.join(__dirname, "views/layouts")}));
+// app.set("view engine", "handlebars")
+// app.set("views", path.join(__dirname, "views"));
+// app.use(express.static('public'))
+// app.get('/', (req, res) => {
+//   res.render('main', {layout : 'userDrinks'});
+//   });
 
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+// app.use(session(sess));
 
-app.use(session(sess));
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
